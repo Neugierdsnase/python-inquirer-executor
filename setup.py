fc18bc262d09dec723f87bdafb6f45383cba8845
k <schwammerlkopf@hotmail.com>
2019-08-07 17:22:10 +0200

    removed doubled url parameter

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setuptools.setup(
     description="A package to tightly bind user choices to functionality. Based on python-inquirer.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
-    packages=setuptools.find_packages(exclude=['tests', 'examples']),
+    url="https://github.com/Neugierdsnase/python-inquirer-executor",
+    packages=setuptools.find_packages(exclude=["tests", "examples"]),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
@@ -25,14 +25,9 @@ setuptools.setup(
         "Topic :: Software Development :: ",
         "Libraries :: Application Frameworks",
     ],
-    url='https://github.com/Neugierdsnase/python-inquirer-executor',
-    license='MIT',
+    license="MIT",
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'blessings == 1.7',
-        'readchar == 2.0.1',
-        'python-editor==1.0.4',
-    ],
-    keywords='color terminal',
+    install_requires=["blessings == 1.7", "readchar == 2.0.1", "python-editor==1.0.4"],
+    keywords="color terminal",
 )
